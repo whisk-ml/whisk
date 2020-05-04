d7c93002b4f896bd81e5fb03441817f3736f52a7
Derek Haynes <derek.haynes@gmail.com>
2020-05-04 06:47:05 -0600

    Removing pname dir, modified tox to remove py3.5

--- pname/core/tasks/destroy/__init__.py
+++
@@ -1,5 +0,0 @@
-from invoke import Collection
-import core.tasks.destroy.s3
-
-ns = Collection()
-ns.add_collection(s3)
--- pname/core/tasks/setup/__init__.py
+++
@@ -1,5 +0,0 @@
-from invoke import Collection
-import core.tasks.setup.s3
-
-ns = Collection()
-ns.add_collection(s3)
