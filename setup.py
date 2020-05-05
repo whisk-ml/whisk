e6a8520cb4d7373d776e618dca8d445e27b2ab3b
Derek Haynes <derek.haynes@gmail.com>
2020-05-05 12:51:58 -0600

    Add back Py36 tests

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Derek Haynes",
     author_email='derek@dlite.cc',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
