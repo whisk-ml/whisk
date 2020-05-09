bf3b804545e3ac5974e56e2102c36efe37727cbc
Derek Haynes <derek.haynes@gmail.com>
2020-05-09 09:28:25 -0600

    Consolidating reqs files, passing GIT* env vars for tox

--- whisk/template/{{ cookiecutter.repo_name }}/setup.py
+++ whisk/template/{{ cookiecutter.repo_name }}/setup.py
@@ -1,22 +1,21 @@
 from setuptools import find_packages, setup
 
-def list_reqs(fname='requirements.txt'):
-    """
-    Adds the packages listed in `fname` as package requirements.
-
-    By default this is the project's main requirements.txt file. To provide
-    a smaller set of packages required only for model predictions, add a
-    `src/requirements.txt` file w/the subset of required packages and load this file instead.
-    """
-    with open(fname) as fd:
-        return fd.read().splitlines()
-
 setup(
     name='{{cookiecutter.project_name}}',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     version='0.1.0',
     include_package_data=True,
+    # By default only whisk is added as a dependency. whisk is required for
+    # accessing whisk.project.artifacts_dir in the default models.model.Model class.
+    # You likely need to list more dependencies for your model package.
+    # For example: your model framework (Scikit, Torch, etc), numpy, Pandas, etc.
+    #
+    # Copying the requirements.txt dependencies into `install_requires=` isn't recommended as this
+    # includes many dependencies that are not required to generate predictions and can result in a very large package.
+    #
+    # You can test that the package works and contains needed dependencies by running `tox` from the
+    # command line. tox tests the package in an isoloated venv.
     install_requires=[
         '{{cookiecutter.whisk_install_requires}}'
     ],
@@ -27,5 +26,7 @@ setup(
     },
     description='A short description of the project.',
     author='Your name (or your organization/company/team)',
+    author_email='you@example.com',
     python_requires='>=3.6',
+    url="https://ADD THE URL TO YOUR PROJECT GITHUB REPO OR DOCS"
 )
