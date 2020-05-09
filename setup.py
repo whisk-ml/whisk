f1c4b00a80b6f38939a2ee3b3299aa467d8703d2
Derek Haynes <derek.haynes@gmail.com>
2020-05-09 09:41:55 -0600

    Bump version: 0.1.9 → 0.1.10

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bookletai/whisk',
-    version='0.1.9',
+    version='0.1.10',
     zip_safe=False,
 )
