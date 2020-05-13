a2463a20fc1e41d6c576e5540db01963275e3944
Derek Haynes <derek.haynes@gmail.com>
2020-05-13 06:39:15 -0600

    Bump version: 0.1.23 → 0.1.24

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bookletai/whisk',
-    version='0.1.23',
+    version='0.1.24',
     zip_safe=False,
 )
