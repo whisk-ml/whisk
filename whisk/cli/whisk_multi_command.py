426b7b85fa99abed659524ef6f45942b6d9c2102
Derek Haynes <derek.haynes@gmail.com>
2020-05-19 16:28:30 -0600

    Addressing flake8 issues in several files

--- whisk/cli/whisk_multi_command.py
+++ whisk/cli/whisk_multi_command.py
@@ -3,13 +3,17 @@ from whisk import whisk
 from whisk.project import Project
 import os
 
+
 class WhiskMultiCommand(click.MultiCommand):
     """
-    This is a custom class based on https://github.com/pallets/click/blob/master/examples/complex/complex/cli.py
-    that loads click command files at run time from both the whisk module commands and commands
-    specified within the created project. This lets users override existing whisk cli commands and create new commands.
+    This is a custom class based on
+    https://github.com/pallets/click/blob/master/examples/complex/complex/cli.py
+    that loads click command files at run time from both the whisk module
+    commands and commands specified within the created project. This lets users
+    override existing whisk cli commands and create new commands.
 
-    Project-specific commands are only loaded if the the cwd is the project root.
+    Project-specific commands are only loaded if the the cwd
+    is the project root.
     """
 
     def core_commands_dir(self):
@@ -33,7 +37,9 @@ class WhiskMultiCommand(click.MultiCommand):
         rv = []
         rv = rv + self._command_file_names(self.core_commands_dir())
         if project.in_project():
-            rv = rv + self._command_file_names(self.core_project_commands_dir())
+            rv = rv + self._command_file_names(
+                                               self.core_project_commands_dir()
+                                              )
             # Possible commands directory is deleted in project
             if project.commands_dir.exists():
                 rv = rv + self._command_file_names(project.commands_dir)
@@ -44,7 +50,8 @@ class WhiskMultiCommand(click.MultiCommand):
     def _command_file_names(self, dir_name):
         """
         Returns a list of file names minus the *.py file extension.
-        This assumes that click commands are listed in directories containing only click command files.
+        This assumes that click commands are listed in directories
+        containing only click command files.
         """
         rv = []
         for filename in os.listdir(dir_name):
@@ -53,7 +60,7 @@ class WhiskMultiCommand(click.MultiCommand):
         rv.sort()
         return rv
 
-    def _eval_file(self,fn):
+    def _eval_file(self, fn):
         """
         Evals the given filename, return the 'cli' namespace.
         The file must contain a 'cli' click command or group.
@@ -71,20 +78,23 @@ class WhiskMultiCommand(click.MultiCommand):
         if 'cli' in ns:
             return ns['cli']
         else:
-            # The code was eval'd but did not have a `cli` key in the namespace.
+            # The code was eval'd but did not have a `cli`
+            # key in the namespace.
             return
 
     def get_command(self, ctx, name):
         """
         MultiCommand classes must implement this function.
 
-        Returns the CLI command w/the given name. The following load order is used:
+        Returns the CLI command w/the given name. The following load order
+        is used:
         * Project-specific commands
         * Core commands
         * Core project-specific commands
 
-        If a command is found it is returned immediately. For example, if both a custom project-specific
-        command and a core project-specific command exist with the same name the custom command is returned,
+        If a command is found it is returned immediately. For example, if
+        both a custom project-specific command and a core project-specific
+        command exist with the same name the custom command is returned,
         overriding the default.
         """
         project = Project()
--- whisk/whisk.py
+++ whisk/whisk.py
@@ -2,14 +2,14 @@
 import whisk
 from whisk.cli.log_tree import PARENT_TREE_NODE_PREFIX, CHILD_TREE_NODE_PREFIX
 from cookiecutter.main import cookiecutter
-import os
-from os.path import dirname, realpath
+from os.path import realpath
 # https://docs.python.org/3/library/pathlib.html
 # Object-oriented filesystem paths
 from pathlib import Path
 import logging
 
 logger = logging.getLogger(__name__)
+
 
 def root_module_dir():
     """
@@ -18,8 +18,10 @@ def root_module_dir():
     filepath = realpath(__file__)
     return Path(filepath).parents[0]
 
+
 def cookiecutter_template_dir():
     return str(root_module_dir() / 'template/')
+
 
 def project_name_to_slug(project_name):
     """
@@ -30,6 +32,7 @@ def project_name_to_slug(project_name):
     """
     return project_name.lower().replace(' ', '_')
 
+
 def create(project_name, output_dir=".", force=False):
     """
     Creates a whisk project.
@@ -37,7 +40,8 @@ def create(project_name, output_dir=".", force=False):
     Parameters
     ----------
     project_name : str
-        Name of the directory to create for the project. This is converted to a slug via :func:`project_name_to_slug`.
+        Name of the directory to create for the project. This is converted to a
+        slug via :func:`project_name_to_slug`.
 
     output_dir : str, optional
         Path to create the directory. Default is the current working directory.
@@ -45,14 +49,15 @@ def create(project_name, output_dir=".", force=False):
     force : bool, optional
         Recreates the project directory if it exists. Default is `False`.
     """
-    # Locks to a specific version as earlier and later versions of whisk could expect a different
-    # template structure.
+    # Locks to a specific version as earlier and later versions of whisk could
+    # expect a different template structure.
     whisk_version = "whisk=={}".format(whisk.__version__)
 
     project_name_slug = project_name_to_slug(project_name)
 
-    # `whisk_dependency` is more flexible (for example, specifying a local install)
-    # than `whisk_install_requires` and is used in testing to require the local version of whisk.
+    # `whisk_dependency` is more flexible (for example, specifying a local
+    # install) than `whisk_install_requires` and is used in testing to require
+    # the local version of whisk.
     extra_content = {
         "project_name": project_name_slug,
         # Added to the project's requirements.txt
@@ -60,13 +65,14 @@ def create(project_name, output_dir=".", force=False):
         # Added to the project's setup.py file
         "whisk_install_requires": whisk_version
     }
-    logger.debug("Creating whisk project with extra_content={}".format(extra_content))
-    logger.info(PARENT_TREE_NODE_PREFIX+"Creating project directory structure...")
+    logger.debug(f"Creating whisk project with extra_content={extra_content}")
+    logger.info(PARENT_TREE_NODE_PREFIX +
+                "Creating project directory structure...")
     res = cookiecutter(cookiecutter_template_dir(),
-                no_input=True,
-                overwrite_if_exists=force,
-                output_dir=output_dir,
-                extra_context=extra_content)
+                       no_input=True,
+                       overwrite_if_exists=force,
+                       output_dir=output_dir,
+                       extra_context=extra_content)
     logger.info(CHILD_TREE_NODE_PREFIX+"Project created in %s", res)
     logger.info(CHILD_TREE_NODE_PREFIX+"DONE.")
     return res
